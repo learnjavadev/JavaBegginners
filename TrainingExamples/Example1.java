6de53b01a5bc92160eda2f9993283ace95e12ec1
Abdulhamid <learn.java.dev@gmail.com>
2024-06-13 23:06:35 +0200

    Create Example1.java

---
+++ TrainingExamples/Example1.java
@@ -0,0 +1,19 @@
+package com.example.mobilephone;
+
+import java.util.Scanner;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+	      System.out.println("Please enter person's data:");
+			
+			System.out.println("Please enter name:");
+			
+			Scanner sc = new Scanner(System.in);
+			
+			String name = sc.next();
+			
+			System.out.println("Name entered is="+name);
+			
+	  }
+}
