371946ec943b9ef4701bd32d2c89656de4e71655
Abdulhamid <learn.java.dev@gmail.com>
2024-06-13 23:00:56 +0200

    Create DataTypeExampleProgram.java

---
+++ TrainingExamples/DataTypeExampleProgram.java
@@ -0,0 +1,59 @@
+
+public class DataTypeExampleProgram {
+	
+	public static void main(String[] args) {
+		
+		// variable declaration
+//		byte number1; // -127 to +128
+//		
+//		System.out.println("This is my data type example program!");
+//		
+//		// assignment x + y = 10
+//		number1 = 100 ;
+//		
+//		System.out.println(number1);
+		
+		// short data type example
+		
+		// variable declration
+//		short number2;
+//		
+//		number2 = 30000;
+//		
+//		System.out.println(number2);
+		
+		// int data type example
+		
+		// variable declaration + assignment in one statement
+//		long number3 = 2147483648l;
+//		
+//		System.out.println("This is value of numer3="+number3);
+//		
+//		number3 = 8888888;
+//		
+//		System.out.println("This is value of numer3="+number3);
+		
+//		double number4 = 123.123456789d;
+//		
+//		number4 = 999.123456789d;
+//		System.out.println(number4);
+		
+		
+//		boolean pass;
+//		
+//		pass = true; // 1 / 0 ;
+//		
+//		pass = false;
+//		
+//		System.out.println("pass="+pass);
+		
+		char alphabet = 'A';
+//		alphabet = '\"';
+		System.out.println(alphabet);
+		
+		
+		
+		
+	
+	}
+}
