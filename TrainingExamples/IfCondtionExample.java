3b7a8994ce7eb1ba2bae52539e2098f05cf74915
Abdulhamid <learn.java.dev@gmail.com>
2024-06-13 23:03:03 +0200

    Create IfCondtionExample.java

---
+++ TrainingExamples/IfCondtionExample.java
@@ -0,0 +1,26 @@
+
+public class IfCondtionExample {
+
+	public static void main(String[] args) {
+		
+//		boolean isItRainingOutSide = true;
+//		
+//		if(isItRainingOutSide) {
+//			System.out.println("I will travel by car!");
+//		}
+		
+//		int a = 11;
+//		
+//		boolean result = a != 0;
+		String str1 = "abc";
+		String str2 = "abc";
+		
+		if(str1 != str2) {
+			System.out.println("This is executed when condition is true");
+		}else {
+			System.out.println("This is executed when condition is false");
+		}
+		
+		System.out.println("End of program!");
+	}
+}
