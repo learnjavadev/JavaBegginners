df499083699a4053930d99cb4676c7ddd944650a
Abdulhamid <learn.java.dev@gmail.com>
2024-06-13 23:03:48 +0200

    Create JavaOperatorsExample.java

---
+++ TrainingExamples/JavaOperatorsExample.java
@@ -0,0 +1,36 @@
+
+public class JavaOperatorsExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is java operators program!");
+		
+		int number1 = 11; // declaration + assignment
+		int number2 = 3; 
+		int number3 = number1 + number2; // addition with operator '+'
+//		
+//		System.out.println("Value of number3="+number3);
+//		
+//		String str1 = "Abdulhamid";
+//		String str2 = "Rashankar";
+//		
+//		System.out.println(str1+" java");
+		
+		
+		number3 = number2 - number1;
+		
+//		System.out.println("value of number3 is="+number3);
+		
+//		number3 = number1 * number2; // multiplication
+//		System.out.println("value of number3 is="+number3);
+		
+		
+//		number3 = number1 / number2; // division
+//		System.out.println("value of number3 is="+number3);
+		
+		// modulus 
+		number3 = number1 % number2; // 11 % 3
+		System.out.println("value of number3 is="+number3);
+		
+	}
+}
