b0a3f606da928ca5cac76c1e594fcb081c66f275
Abdulhamid <learn.java.dev@gmail.com>
2024-06-13 23:08:43 +0200

    Update Person.java

--- TrainingExamples/Person.java
+++ TrainingExamples/Person.java
@@ -1,8 +1,24 @@
 
 public class Person {
-	
-	String name;
-	int height;
-	long contactNumber;
 
+	// characteristics / states / fields
+	String name; // "Abdulhamid"
+	int age; // 32
+	
+	
+	// behaviors / method / function
+	public void walk() {
+		System.out.println("I can walk!");
+		// statement 2
+		// ....
+	}
+	
+	public void printPersonDetails() {
+		System.out.println("Name of the person is="+name);
+		System.out.println("Age of the person is="+age);
+	}
+	
+	public static void staticFunction() {
+		System.out.println("This is static function");
+	}
 }
