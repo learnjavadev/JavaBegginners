224a0134f7763c04282915326b3d6451266e27d6
Abdulhamid <learn.java.dev@gmail.com>
2024-06-13 23:03:34 +0200

    Create IncrementAndDecrementOperatorExample.java

---
+++ TrainingExamples/IncrementAndDecrementOperatorExample.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class IncrementAndDecrementOperatorExample {
+
+	public static void main(String[] args) {
+		
+//		long number1 = 10l;
+//		char result = 'A'; 
+//		--result;
+//		System.out.println(result);
+//		number1 = number1 + 1 ;
+		
+//		number1++;
+		
+//		System.out.println(number1++);
+//		System.out.println(number1);
+		
+//		System.out.println(++number1);
+//		System.out.println(number1);
+//		
+//		System.out.println(--number1);
+//		System.out.println(number1);
+		
+		int number = 10;
+		number = number + 1; // number++
+		System.out.println("value of number="+ ++number);
+		System.out.println("value of number="+number);
+		
+		
+	}
+}
