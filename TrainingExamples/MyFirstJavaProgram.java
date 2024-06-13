65d04d7e6949e90606fc9176d60734f2fead6535
Abdulhamid <learn.java.dev@gmail.com>
2024-06-13 23:04:18 +0200

    Create MyFirstJavaProgram.java

---
+++ TrainingExamples/MyFirstJavaProgram.java
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//#include <conio.h>
+
+
+
+public class MyFirstJavaProgram {
+
+	public static void main(String[] abc) {
+		
+		System.out.println("This is my first program!");
+	}
+}
