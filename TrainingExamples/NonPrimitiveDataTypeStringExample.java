8a2b4f3164afdc817f12cabe9c804dca9da2c342
Abdulhamid <learn.java.dev@gmail.com>
2024-06-13 23:04:54 +0200

    Create NonPrimitiveDataTypeStringExample.java

---
+++ TrainingExamples/NonPrimitiveDataTypeStringExample.java
@@ -0,0 +1,17 @@
+
+public class NonPrimitiveDataTypeStringExample {
+	
+	public static void main(String[] args) {
+		System.out.println("This is start of program");
+		// declaration + assignment
+        String name = null;
+        
+        // re-assignment
+		name = "ABCD";
+		
+        System.out.println(name);
+        
+		System.out.println("This is end of program");
+	}
+
+}
