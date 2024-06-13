6ff35653ab029ec5c7cd75be25bf1146147c2151
Abdulhamid <learn.java.dev@gmail.com>
2024-06-13 22:59:53 +0200

    Create ComparisonOperatorsExample.java

---
+++ TrainingExamples/ComparisonOperatorsExample.java
@@ -0,0 +1,17 @@
+
+public class ComparisonOperatorsExample {
+
+	public static void main(String[] args) {
+		
+		int number1 = 10;
+		int number2 = 10;
+
+		// 10 >= 10 -> true
+		if(number1 != number2) {
+			System.out.println("Both numbers are NOT equal");
+			// 
+		}
+		
+		System.out.println("End of the program");
+	}
+}
